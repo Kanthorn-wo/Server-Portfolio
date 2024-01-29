2fa5b282fe908253bf3128bbafba0674fb57f01d
Kanthorn1995 <kanthorn.wo@rmuti.ac.th>
2024-01-29 15:06:20 +0700

    cinfig DB

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/demo')
+        // await mongoose.connect('mongodb://127.0.0.1:27017/demo')
+        await mongoose.connect('mongodb+srv://kanthornwo:rmhBgbIoozRRrypD@cluster0.wukdrub.mongodb.net/demo')
         console.log('DB Connected')
     } catch (err) {
         console.log(err)
