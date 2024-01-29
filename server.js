658209cb8d8bc1977db65754de49691b75303216
Kanthorn1995 <kanthorn.wo@rmuti.ac.th>
2024-01-29 16:00:19 +0700

    fix cors

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ const app = express();
 connectDB()
 
 app.use(morgan('dev'))
-app.use(cors())
+app.use(cors({
+    origin: 'https://jarmoo-portfolio-alpha.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
 app.use(bodyParse.json({ limit: '10mb' }))
 
 // Route 1
