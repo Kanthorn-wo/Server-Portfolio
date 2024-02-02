59d2acaba67b5bbf40999cd7338bd33431f7d6d2
Kanthorn1995 <kanthorn.wo@rmuti.ac.th>
2024-02-03 00:40:12 +0700

    fix cors 7

--- server.js
+++ server.js
@@ -9,11 +9,23 @@ const app = express();
 
 connectDB()
 
-app.use(morgan('dev'))
+//app.use(morgan('dev'))
 app.use(cors())
 app.options("*", cors);
 app.use(bodyParse.json({ limit: '10mb' }))
 
+// Middleware to handle CORS for specific routes
+app.use('/api', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins, you may want to specify only trusted origins
+    res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+    // Allow credentials (e.g., cookies)
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    // Continue to the next middleware
+    next();
+});
 
 readdirSync('./Routes')
     .map((r) => app.use('/api', require('./Routes/' + r)))
