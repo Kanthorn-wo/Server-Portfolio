f2e08fa92ba25978ba053192900bd33a902b23cf
Kanthorn1995 <kanthorn.wo@rmuti.ac.th>
2024-02-03 00:27:44 +0700

    fix cors 7

--- server.js
+++ server.js
@@ -3,42 +3,18 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyParse = require('body-parser')
-
 const connectDB = require('./Config/db')
-
-
 const { readdirSync } = require('fs')
-// const productRouters = require('./Routes/product')
-// const authRouters = require('./Routes/auth')
-const corsOptions = {
-    origin: 'https://jarmoo-portfolio-alpha.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-};
-
 const app = express();
 
 connectDB()
 
 app.use(morgan('dev'))
-app.use(cors(corsOptions))
+app.use(cors())
+app.options("*", cors);
 app.use(bodyParse.json({ limit: '10mb' }))
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
 
-// Route 1
-// app.get('/product', (req, res) => {
-//     res.send('Hello Endpoint 555')
-// })
-
-// Route 2
-// app.use('/api', productRouters)
-// app.use('/api', authRouters)
-
-// Route 3
 readdirSync('./Routes')
     .map((r) => app.use('/api', require('./Routes/' + r)))
 
